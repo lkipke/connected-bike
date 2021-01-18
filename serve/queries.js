069fe858b1f57a988aea917a7975ae6d8d242686
Levi Kipke <kipkelevi23@gmail.com>
2021-01-18 15:08:18 -0800

    remove pool

--- serve/queries.js
+++ serve/queries.js
@@ -1,5 +1,20 @@
+const { Pool } = require('pg');
 const format = require('pg-format');
-const { getPool } = require('./util/pool');
+
+let pool;
+function getPool() {
+    if (!pool) {
+        pool = new Pool({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PWD,
+            database: process.env.DB_NAME,
+        });
+    }
+
+    return pool;
+}
 
 async function insertPing(variables) {
     let query = format(
